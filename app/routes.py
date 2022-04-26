8f948564f1e2fc88a9be684b089902ea0e996767
mit <mit.magh@gmail.com>
2022-04-26 11:23:31 -0700

    get one planet

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
+
 
 class Planet:
     def __init__(self, id, name, description, distance_from_sun):
-        self.id = id,
-        self.name = name,
-        self.description = description,
+        self.id = id
+        self.name = name
+        self.description = description
         self.distance_from_sun = distance_from_sun
 
 planets = [
@@ -32,3 +33,50 @@ def get_planest():
         })
 
     return jsonify(planets_response), 200
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"msg": f"Planet {planet_id} is invalid"}, 400))
+
+    chosen_planet = None
+    for planet in planets:
+        print(f"{planet_id}", type(planet.id))
+        if planet.id == planet_id:
+            chosen_planet = planet
+            break
+    if chosen_planet is None:
+        rsp = {"msg": f"Planet {planet_id} is not found"}
+        return jsonify(rsp), 404
+    rsp = {
+        "id": chosen_planet.id,
+        "name": chosen_planet.name,
+        "description":  chosen_planet.description,
+        "distance_from_sun" : chosen_planet.distance_from_sun
+    }
+    return jsonify(rsp), 200
+
+
+
+
+
+
+
+
+
+
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
+#     abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+# @planets_bp.route("/<planet_id>", methods=["GET"])
+# def read_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
+#     return jsonify(planet.to_json(), 200)
