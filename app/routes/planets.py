90b9d901b9265bb363deb681d84810bb960c19ca
Victoria Shade <victoria.k.shade@gmail.com>
2022-05-03 11:52:27 -0700

    Wave04 successfully completed

--- app/routes/planets.py
+++ app/routes/planets.py
@@ -24,7 +24,7 @@ def create_new_planet():
     db.session.add(new_planet)
     db.session.commit()
 
-    return {"msg": f"Successfully created new planet with id {new_planet.id}"}, 201
+    return {"message": f"Successfully created new planet with id {new_planet.id}"}, 201
 
 
 @planets_bp.route("", methods=["GET"])
@@ -41,9 +41,93 @@ def get_all_planets():
 
     return jsonify(planets_response), 200
 
+@planets_bp.route('<planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        rsp = {"message" : f"Invalid id: {planet_id}"}
+        return jsonify(rsp), 400
+    chosen_planet = Planet.query.get(planet_id)
+
+    if chosen_planet is None:
+        return {"message": f"planet {planet_id} is not found"}, 404
+
+    planet_response = {
+        "id": chosen_planet.id,
+        "name": chosen_planet.name,
+        "description":  chosen_planet.description,
+        "distance_from_sun" : chosen_planet.distance_from_sun 
+        }
+
+    return jsonify(planet_response), 200
+
+@planets_bp.route('/<planet_id>', methods=['PUT'])
+def update_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        rsp = {"message" : f"Invalid id: {planet_id}"}
+        return jsonify(rsp), 400
+    
+    chosen_planet = Planet.query.get(planet_id)
+
+    if chosen_planet is None:
+        return {"message": f"planet {planet_id} is not found"}, 404
+
+    request_body = request.get_json()
+    try:
+        chosen_planet.name=request_body["name"],
+        chosen_planet.description=request_body["description"],
+        chosen_planet.distance_from_sun=request_body["distance_from_sun"]
+
+        db.session.commit()
+
+    except KeyError:
+        return {"message": "name, description, and distance from sun are required"}, 400    
+
+    return {"message": f"Successfully updated planet with id {chosen_planet.id}"}, 200
 
 
+@planets_bp.route("/<planet_id>", methods=['DELETE'])
+def delete_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        rsp = {"message" : f"Invalid id: {planet_id}"}
+        return jsonify(rsp), 400
 
+    chosen_planet = Planet.query.get(planet_id)
+
+    if chosen_planet is None:
+        return {"message": f"planet {planet_id} is not found"}, 404
+
+    db.session.delete(chosen_planet)
+    db.session.commit()
+
+    return {
+        "message": f"planet #{planet_id} successfully deleted"
+    }, 200
+
+# @cats_bp.route("/<cat_id>", methods=["DELETE"])
+# def delete_cat(cat_id):
+#     try:
+#         cat_id = int(cat_id)
+#     except ValueError:
+#         rsp = {"msg": f"Invalid id: {cat_id}"}
+#         return jsonify(rsp), 400
+    
+#     chosen_cat = Cat.query.get(cat_id)
+#     if chosen_cat is None:
+#         rsp = {"msg": f"Could not find cat with id {cat_id}"}
+#         return jsonify(rsp), 404
+
+#     db.session.delete(chosen_cat.id)
+#     db.session.commit()
+
+#     return {
+#         "msg": f"cat #{chosen_cat.id} successfully destroyed"
+#     }, 200
 
 
 
